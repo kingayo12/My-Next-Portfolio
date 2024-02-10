538550a4255e07f6e8b13b23c1bfa4c6bb3a6eea
kingayo12 <oreoluwaa08@gmail.com>
2024-02-10 18:02:44 +0100

    push update

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,82 +1,72 @@
 //import swiper react
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 // import modules
 import { FreeMode, Pagination } from "swiper";
 
 // icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-  RxArrowTopRight,
-} from "react-icons/rx";
-
+import { RxCrop, RxPencil2, RxDesktop, RxReader, RxRocket, RxArrowTopRight } from "react-icons/rx";
 
 // data
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description:
+      "Conveying your business essence through visuals and messaging, fostering trust and connection with audiences and Partners.",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description:
+      " delivering creativity into tangible solutions, harmonizing form and function for meaningful experiences.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description:
+      "Transforming concepts into reality through innovation, iteration, and technical mastery for impactful solutions.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description:
+      "Craft captivating narratives, guiding audiences on engaging journeys and inspiring action with compelling storytelling.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description:
+      "Optimize online presence by ensuring visibility through strategic techniques and enhancing digital success.",
   },
 ];
 
-
-
-
-
-
 const ServiceSlider = () => {
-  return(
-    <Swiper 
-    breakpoints={{
-      320:{
-        slidesPerView:1,
-        spaceBetween: 15,
-      },
-      640:{
-        slidesPerView:3,
-        spaceBetween: 15,
-      },
-    }}
-    freeMode={true}
-    pagination={{
-      clickable: true
-    }}
-    modules={[FreeMode, Pagination]}
-    className="h-[240px] sm:h-[340px]"
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className='h-[240px] sm:h-[340px]'
     >
       {serviceData.map((item, index) => {
-        return(
+        return (
           <SwiperSlide key={index}>
-            <div className=" bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg[rgba(89,65,160,0.15)] transition-all duration-300">
+            <div className=' bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg[rgba(89,65,160,0.15)] transition-all duration-300'>
               {/* icon  */}
               <div> {item.icon} </div>
               {/* title & desc  */}
@@ -85,13 +75,12 @@ const ServiceSlider = () => {
                 <div className='max-w-[350px] leading-normal'> {item.description} </div>
               </div>
               {/* arrow  */}
-              <div className="text-3xl">
+              <div className='text-3xl'>
                 <RxArrowTopRight className=' group-hover:rotate-45 group-hover:text-accent transition-all duration-300' />
               </div>
             </div>
-        </SwiperSlide>
-        )
-        
+          </SwiperSlide>
+        );
       })}
     </Swiper>
   );
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,63 +1,90 @@
 import Circles from "../../components/Circles";
 import WorkSlider from "../../components/WorkSlider";
 import Bulb from "../../components/Bulb";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 //farmer
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 //variant
-import {fadeIn} from '../../variants'
+import { fadeIn } from "../../variants";
 //icons
 import { BsArrowRight } from "react-icons/bs";
 
-
 const Contact = () => {
-  return(
-    <div className="h-full bg-primary/30">
-      <Circles/>
-      <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full ">
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_rvy5ztc", "template_iznui1s", form.current, "fMUbpbkAXKitGy6dT")
+      .then((result) => {
+        console.log(result.text);
+        form.current.reset();
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
+
+  return (
+    <div className='h-full bg-primary/30'>
+      <Circles />
+      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full '>
         {/* text & form  */}
-        <div className="flex flex-col w-full max-w-[700px]">
+        <div className='flex flex-col w-full max-w-[700px]'>
           {/* text  */}
           <motion.h2
-           variants= {fadeIn('up', 0.2 )}
-           initial='hidden'
-           animate='show'
-           exit='hidden'
-          className=" h2 text-center mb-12">
-            Let's <span className="text-accent">connect.</span>
+            variants={fadeIn("up", 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className=' h2 text-center mb-12'
+          >
+            Let's <span className='text-accent'>connect.</span>
           </motion.h2>
 
           {/* form  */}
           <motion.form
-           variants= {fadeIn('down', 0.2 )}
-           initial='hidden'
-           animate='show'
-           exit='hidden'
-           className="flex-1 flex flex-col gap-6 w-full mx-auto">
+            variants={fadeIn("down", 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='flex-1 flex flex-col gap-6 w-full mx-auto'
+            onSubmit={sendEmail}
+            ref={form}
+          >
             {/* input group  */}
-            <div className="flex gap-x-6 w-full">
-              <input type="text" placeholder="name" className="input"/>
-              <input type="email" placeholder="email" className="input"/>
+            <div className='flex gap-x-6 w-full'>
+              <input type='text' placeholder='name' name='from_name' className='input' />
+              <input
+                type='text'
+                placeholder='name'
+                name='to_name'
+                className='input hidden'
+                value=''
+                Kings
+              />
+              <input type='email' placeholder='email' name='email' className='input' />
             </div>
-            <input type="text" placeholder="subject" className="input"/>
-            <textarea className="textarea" placeholder="message"></textarea>
+            <input type='text' placeholder='subject' name='subject' className='input' />
+            <textarea className='textarea' name='message' placeholder='message'></textarea>
 
             {/* button  */}
-            <button 
-            className=" btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
+            <button
+              className=' btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'
+              type='submit'
             >
-              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>
                 Let's talk
               </span>
-              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all"/>
+              <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all' />
             </button>
           </motion.form>
         </div>
-       
       </div>
-
     </div>
-
-  )};
+  );
+};
 
 export default Contact;
